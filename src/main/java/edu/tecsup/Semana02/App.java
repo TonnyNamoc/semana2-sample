c4c704750251ce0487fb499c8835bd0f2a574294
Tonny Alexander Namoc Julca <tonny.namoc@tecsup.edu.pe>
2025-03-24 17:03:19 -0500

    Eso tilin

--- src/main/java/edu/tecsup/Semana02/App.java
+++ src/main/java/edu/tecsup/Semana02/App.java
@@ -1,11 +1,54 @@
 package edu.tecsup.Semana02;
 
+import java.util.Scanner;
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Salut le gars!");
-        System.out.println("El pepe");
-    }
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("Bienvenido a la calculadora básica");
+	        System.out.print("Ingresa el primer número: ");
+	        double num1 = scanner.nextDouble();
+
+	        System.out.print("Ingresa el segundo número: ");
+	        double num2 = scanner.nextDouble();
+
+	        System.out.print("Elige una operación (+, -, *, /): ");
+	        String operacion = scanner.next();
+
+	        double resultado = 0;
+	        boolean operacionValida = true;
+
+	        switch (operacion) {
+	            case "+":
+	                resultado = num1 + num2;
+	                break;
+	            case "-":
+	                resultado = num1 - num2;
+	                break;
+	            case "*":
+	                resultado = num1 * num2;
+	                break;
+	            case "/":
+	                if (num2 != 0) {
+	                    resultado = num1 / num2;
+	                } else {
+	                    System.out.println("No se puede dividir por cero.");
+	                    operacionValida = false;
+	                }
+	                break;
+	            default:
+	                System.out.println("Operación no válida.");
+	                operacionValida = false;
+	                break;
+	        }
+
+	        if (operacionValida) {
+	            System.out.println("Resultado: " + resultado);
+	        }
+
+	        scanner.close();
+	    }
 }
