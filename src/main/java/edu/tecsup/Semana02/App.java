95d539061a5c1c6f73c6be776446f921a189aad6
Tonny Alexander Namoc Julca <tonny.namoc@tecsup.edu.pe>
2025-03-24 16:55:49 -0500

    El pepe

--- src/main/java/edu/tecsup/Semana02/App.java
+++ src/main/java/edu/tecsup/Semana02/App.java
@@ -6,5 +6,6 @@ package edu.tecsup.Semana02;
 public class App {
     public static void main(String[] args) {
         System.out.println("Salut le gars!");
+        System.out.println("El pepe");
     }
 }
